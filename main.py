17e7188a6859b4af6b5063dc8437624be6ba6efb
Gaurav Kulkarni <gkulk007@gmail.com>
2020-09-07 15:30:30 +0530

    base created

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import io
+import pygame
+import textwrap
+import threading
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import ttk
+from gtts import gTTS
+from googletrans import Translator
+from PyDictionary import PyDictionary
